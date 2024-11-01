27a315e4752447d70eae524ad3f103247b23baca
renaud.beuret <renaud.beuret@etu.uca.fr>
2024-11-01 12:36:06 +0100

    [ADD] Init Db

--- src/MongoFunctions.py
+++ src/MongoFunctions.py
@@ -10,11 +10,12 @@ def get_database():
 
 # Importe un doucment csv dans la base Mongo
 # et retourne le nombre d'élément dans 
-def import_csv_file(database, coll_name, file_path):
+def import_csv_file(database, coll_name, file_path, separator="\t"):
     collection = database[coll_name]
+    collection.delete_many({})
     data = open(file_path, 'r', encoding='utf-8')
 
-    reader = csv.DictReader(data, delimiter='\t')
+    reader = csv.DictReader(data, delimiter=separator)
     header = reader.fieldnames
 
     count = 0
@@ -27,3 +28,12 @@ def import_csv_file(database, coll_name, file_path):
         count += 1
 
     return count
+
+# Rentre les données dans la bd depuis les fichiers csv.
+# Retourne un dictionnaire avec le nombre d'insertions pour chaque
+# collection
+def init_db():
+    results = {}
+    db = get_database()
+    results['users'] = import_csv_file(db, 'users', './Data/tw_user.csv')
+    results['tweets'] = import_csv_file(db, 'tweets', './Data/tweet.csv','<')
--- src/jsp.py
+++ src/jsp.py
@@ -1,6 +1,4 @@
 import MongoFunctions as mongo
 
-aura = "VmaizN0uVsUGTIbu6-0H_aYXsexRwxrwpD_siLuRCOs"
-
 db = mongo.get_database()
-print(mongo.import_csv_file(db, 'users', './Data/tw_user.csv'))
+mongo.init_db()
