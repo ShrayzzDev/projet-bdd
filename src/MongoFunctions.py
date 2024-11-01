c06acb8fc187f4b34cb28a74ffebbf0d712ec066
renaud.beuret <renaud.beuret@etu.uca.fr>
2024-11-01 15:40:01 +0100

    [ADD] Get/Add/Del user et tweet (user testé mais pas tweet)

--- src/MongoFunctions.py
+++ src/MongoFunctions.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 import pandas as pd
 import csv
+from datetime import datetime
 
 # Se connecte à la base en cloud et nous la retourne
 def get_database():
@@ -37,3 +38,59 @@ def init_db():
     db = get_database()
     results['users'] = import_csv_file(db, 'users', './Data/tw_user.csv')
     results['tweets'] = import_csv_file(db, 'tweets', './Data/tweet.csv','<')
+
+def add_user(id_user, screen_name, name, description, location, lang):
+    user = {
+        'idUser' : id_user,
+        'screenName' : screen_name,
+        'name' : name,
+        'description' : description,
+        'location' : location,
+        'lang' : lang,
+        'url' : 'https://twitter.com/' + screen_name,
+        'createdAt' : datetime.today().strftime("DD/MM/YY"),
+        'nbStatuses' : 0,
+        'nbFavorites' : 0,
+        'nbFollowers' : 0,
+        'nbFollowing' : 0
+    }
+    db = get_database()
+    collection = db['users']
+    collection.insert_one(user)
+
+def delete_user(id_user):
+    db = get_database()
+    collection = db['users']
+    collection.delete_one({ "idUser" : id_user })
+
+def get_user_by_id(user_id):
+    db = get_database()
+    collection = db['users']
+    return collection.find_one({ "idUser" : user_id })
+
+def add_tweet(id_tweet, id_user, text, source, lang, reply_id_tweet = "", reply_id_user = "", quoted_id_tweet = "", quoted_id_user = ""):
+    user = get_user_by_id(id_user)
+    tweet = {
+        'idTweet' : id_tweet,
+        'idUser' : id_user,
+        'replyIdTweet' : reply_id_tweet,
+        'replyIdUser' : reply_id_user,
+        'quotedIdTweet' : quoted_id_tweet,
+        'quotedIdUser' : quoted_id_user,
+        'text' : text,
+        'createdAt' : datetime.today().strftime("DD/MM/YY"),
+        'url' : 'https://twitter.com/' + user['screenName'] + '/status/' + id_tweet,
+        'source' : source,
+        'lang' : lang,
+        'nbRetweet' : 0,
+        'nbFavorites' : 0
+    }
+    db = get_database()
+    collection = db['tweets']
+    collection.insert_one(tweet)
+
+def delete_user(id_tweet):
+
+    db = get_database()
+    collection = db['tweets']
+    collection.delete_one({ "idTweet" : id_tweet })
--- src/jsp.py
+++ src/jsp.py
@@ -1,4 +1,9 @@
 import MongoFunctions as mongo
+import Neo4JFunctions as neo4j
 
-db = mongo.get_database()
-mongo.init_db()
+# mongo.init_db()
+print(mongo.get_user_by_id("262671176"))
+mongo.add_user("040112","shrayzz","moi","guilty gear c'est très bien", "clermont", "fr")
+print(mongo.get_user_by_id("040112"))
+mongo.delete_user("040112")
+neo4j.init_connection()
