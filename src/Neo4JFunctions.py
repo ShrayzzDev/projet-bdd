ed384ffb9da0881c9aca895148b168e5eda06e17
renaud.beuret <renaud.beuret@etu.uca.fr>
2024-11-01 17:44:34 +0100

    [ADD] Persons dans neo4j (juste nom et id)

--- src/Neo4JFunctions.py
+++ src/Neo4JFunctions.py
@@ -1,10 +1,9 @@
 import dotenv
 import os
 from neo4j import GraphDatabase
+import csv
 
-URI = ""
-AUTH = ("","")
-
+# Charge les identifiants et vérifie que la connexion est possible
 def init_connection():    
     load_status = dotenv.load_dotenv("./Data/neo4j_credentials.txt")
     if load_status is False:
@@ -16,3 +15,24 @@ def init_connection():
     with GraphDatabase.driver(URI, auth=AUTH) as driver:
         driver.verify_connectivity()
         print("Connection established.")
+
+# Importe un doucment csv dans la base Mongo
+# et retourne le nombre d'élément dans 
+def import_persons_csv(file_path, separator="\t"):
+    URI = os.getenv("NEO4J_URI")
+    AUTH = (os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD"))
+    data = open(file_path, 'r', encoding='utf-8')
+
+    reader = csv.DictReader(data, delimiter=separator)
+
+    count = 0
+
+    with GraphDatabase.driver(URI, auth=AUTH) as driver:
+        for each in reader:
+            driver.execute_query(
+                "CREATE (:Person {name: $name, id: $id})",
+                name=each['screenName'],
+                id=each['idUser']
+            )
+
+    return count
--- src/jsp.py
+++ src/jsp.py
@@ -2,13 +2,14 @@ import MongoFunctions as mongo
 import Neo4JFunctions as neo4j
 from datetime import datetime
 
-mongo.init_db()
-mongo.add_user("040112","shrayzz","moi","guilty gear c'est très bien", "clermont", "fr")
-print(mongo.get_user_by_id("040112"))
-mongo.add_tweet("040112","040112","elphelt = queen", "mon pc", "fr")
-print(mongo.get_tweet_by_id("040112"))
-mongo.delete_user("040112")
-print(mongo.get_user_by_id("040112"))
-mongo.delete_tweet("040112")
-print(mongo.get_tweet_by_id("040112"))
+# mongo.init_db()
+# mongo.add_user("040112","shrayzz","moi","guilty gear c'est très bien", "clermont", "fr")
+# print(mongo.get_user_by_id("040112"))
+# mongo.add_tweet("040112","040112","elphelt = queen", "mon pc", "fr")
+# print(mongo.get_tweet_by_id("040112"))
+# mongo.delete_user("040112")
+# print(mongo.get_user_by_id("040112"))
+# mongo.delete_tweet("040112")
+# print(mongo.get_tweet_by_id("040112"))
 neo4j.init_connection()
+neo4j.import_persons_csv('./Data/tw_user.csv')
